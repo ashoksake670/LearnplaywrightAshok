b20aec862288c180ca09da2d7e2307a6cca731ab
ashoksake670 <you@example.com>
2025-09-12 17:44:38 +0530

    Add login test spec

---
+++ tests/Login.spec.js
@@ -0,0 +1,47 @@
+import {test, expect} from '@playwright/test';
+
+test ('Verify login function', async ({page})=>{
+
+await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+await page.locator("//input[@placeholder='Username']").fill ('Admin')
+await page.locator("//input[@placeholder='Password']").fill ('admin123')
+await page.locator("//button[@type='submit']").click ()
+
+await expect(page).toHaveURL ('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+// Dashbord to be visible
+await expect (page.locator("//h6[text()='Dashboard']")).toBeVisible()
+
+})
+
+test ('Verify log with valid user name and invalid password', async ({page})=>{
+
+await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+await page.locator("//input[@placeholder='Username']").fill ('Admin')
+await page.locator("//input[@type='password']").fill ('adhin')
+await page.locator("//button[@type='submit']"). click ()
+
+await expect(page.locator("//p[text()='Invalid credentials']")).toBeVisible()
+
+})
+
+
+test ('Verify log with invalid user name and valid password', async({page})=>{
+
+await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+await page.locator("//input[@placeholder='Username']").fill ('raju')
+await page.locator("//input[@type='password']").fill ('admin123')
+await page.locator("//button[@type='submit']").click ()
+await expect(page.locator("//p[text()='Invalid credentials']")).toBeVisible()
+
+})
+
+test ('Verify log with invalid user name and invalid password', async ({page})=>{
+
+await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+await page.locator("//input[@placeholder='Username']"). fill ('ashok')
+await page.locator("//input[@placeholder='Password']"). fill ('Prem')
+await page.locator("//button[@type='submit']"). click ()
+await expect(page.locator("//p[text()='Invalid credentials']")).toBeVisible
+
+
+})
