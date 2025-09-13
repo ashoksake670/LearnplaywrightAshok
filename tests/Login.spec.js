ae8a4265eba9412797f0b978e9020be8398bce37
ashoksake670 <you@example.com>
2025-09-13 13:58:25 +0530

    save

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -56,11 +56,11 @@ await page.locator('//button[@type="submit"]').click ()
 await expect(page.locator("//h6[text()='Dashboard']")).toBeVisible()
 
 await page.locator('https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPimModule')
-await page.locator("//a[text()='Add Employee']").click()
+await page.locator("//a[text()='Add Employee']").click();
 await page.locator('//input[@name="firstName"]').fill ('john')
 await page.locator('//input[@name="middleName"]').fill ('max')
 await page.locator('//input[@name="lastName"]').fill ('Lewis')
-await page.locator('//button[@type="submit"]').click()
+await page.locator('//button[@type="submit"]').click ()
 
 await expect(page.locator("//h6[text()='Personal Details']")).toBeVisible()
 
